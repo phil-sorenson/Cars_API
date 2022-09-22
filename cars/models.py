fc3442393efccf1cdc7090e2ca4f64b2c4450942
phil-sorenson <phillip.sorenson31@gmail.com>
2022-09-22 00:23:44 -0500

    updated settings.py-installed apps and migrated car model to MySQL

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+# # Representations of the table and the database
 # Create your models here.
+            # models.Model (prepackaged Django function)
+class Car(models.Model):
+    make = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    year = models.IntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
+# # Where we will define all of the functions that the outside world can interact with through URLs over internet
 # Create your views here.
