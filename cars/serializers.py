c0f83729eb7a6734ab14739eebd29d4890d347e1
phil-sorenson <phillip.sorenson31@gmail.com>
2022-09-22 00:52:45 -0500

    saved information in car serializer.py Class and File

---
+++ cars/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Car
+
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = ['id', 'make', 'model', 'year', 'price']   # If one of the fields was taken out, the outside world would never be able to see it (E.g 'make')
+        
