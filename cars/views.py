79f08f5d7058609bcc1fdd0ac985188ce479afa0
phil-sorenson <phillip.sorenson31@gmail.com>
2022-09-23 00:13:04 -0500

    Created a seperate serializer to allow a 'POST' request + added a third car to our table via Postman

--- cars/views.py
+++ cars/views.py
@@ -6,23 +6,18 @@ from .models import Car
 # Create your views here.
 
 # Create a function to define what request types that ifunction is capable of handling (every api_view func. should take in a request)
-@api_view(['GET'])
-def cars_list(resuest):
-    cars = Car.objects.all()
- 
-    serializer = CarSerializer(cars, many=True)
-    return Response(serializer.data) 
+@api_view(['GET','POST'])
 
-# First response after running server:
-    # Page not found (404)
-    # Request Method:	GET
-    # Request URL:	http://127.0.0.1:8000/cars
-    # Using the URLconf defined in car_project.urls, Django tried these URL patterns, in this order:
-
-    # admin/
-    # The current path, cars, didn’t match any of these.
-
-    # You’re seeing this error because you have DEBUG = True in your Django settings file. Change that to False, and Django will display a standard 404 page.
-
-        # REASON FOR THIS ---> cars app and car_project both have urls.py files TO FIX ---> Register our cars urls.py file with our project urls.py file
-            # 
+def cars_list(request):
+    if request.method == 'GET':
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        return Response(serializer.data) 
+    
+    elif request.method == 'POST':
+        serializer = CarSerializer(data = request.data)
+        if serializer.is_valid() == True:               # <--- Code to validate the request coming in via Postman or Frontend 
+            serializer.save()   # QUESTION -- What is the purpose of the .save() function and WHY our own status codes??
+            return Response(serializer.data, status=201) 
+        else:
+            return Response(serializer.errors, status=400)   # <--- setting our own 'Status Code' 
