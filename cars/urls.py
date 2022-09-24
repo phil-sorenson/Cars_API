2980fc17d474740c88ce6836ff76c063e435a691
phil-sorenson <phillip.sorenson31@gmail.com>
2022-09-23 21:45:14 -0500

    Created new View Function for getting cars by ID + added new URLSpath for car detail view function

--- cars/urls.py
+++ cars/urls.py
@@ -3,6 +3,7 @@ from . import views  # WHY JUST 'from ." ??
 
 urlpatterns = [
     path('', views.cars_list),
+    path('<int:pk>/', views.car_detail),    # < > are VERY important--allows item to be passed
 
 ]
 
